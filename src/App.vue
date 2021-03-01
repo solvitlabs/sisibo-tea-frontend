d1fbb931e85adadb10b93db9ce90bde881af5113
d3vkk <donaldkk22@yahoo.com>
2021-03-01 17:11:04 +0300

    refactor: Add home view route

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <template>
   <div>
-    <Navbar />
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Navbar from "./layout/Navbar";
-
 export default {
   name: "App",
-  components: {
-    Navbar,
-  },
 };
 </script>
 
