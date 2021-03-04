2d4d05cfe5819e80839711c540783a88a753b3ea
d3vkk <donaldkk22@yahoo.com>
2021-03-04 15:13:49 +0300

    chore: Beautify code

--- server/lib/config.example.js
+++ server/lib/config.example.js
@@ -2,52 +2,52 @@
  * Create and export configurtion variables
  */
 
- // Container for all the environments
- let environments = {}
+// Container for all the environments
+let environments = {}
 
- // Staging (default) environment
- environments.staging = {
-  httpPort:3000,
-  httpsPort:3001,
-  envName:'staging',
-  hashingSecret:'',
-  twilio:{
-  accountSid : '',
-  authToken : '',
-  fromPhone : ''
+// Staging (default) environment
+environments.staging = {
+  httpPort: 3000,
+  httpsPort: 3001,
+  envName: 'staging',
+  hashingSecret: '',
+  twilio: {
+    accountSid: '',
+    authToken: '',
+    fromPhone: ''
   },
-  database:{
+  database: {
     name: 'sisibotea',
-    host:'localhost',
-    user:'',
-    password:''
+    host: 'localhost',
+    user: '',
+    password: ''
   },
- }
+}
 
- //Production environment
- environments.production = {
-   httpPort:5000,
-   httpsPort:5001,
-   envName:'production',
-   hashingSecret:'',
-   twilio:{
-    accountSid : '',
-    authToken : '',
-    fromPhone : ''
+//Production environment
+environments.production = {
+  httpPort: 5000,
+  httpsPort: 5001,
+  envName: 'production',
+  hashingSecret: '',
+  twilio: {
+    accountSid: '',
+    authToken: '',
+    fromPhone: ''
   },
-  database:{
+  database: {
     name: 'sisibotea',
-    host:'localhost',
-    user:'',
-    password:''
+    host: 'localhost',
+    user: '',
+    password: ''
   },
- }
+}
 
- // Determine which environment was passed as a command-line argument
- let currentEnvironment = typeof process.env.NODE_ENV == 'string' ? process.env.NODE_ENV.toLowerCase():''
+// Determine which environment was passed as a command-line argument
+let currentEnvironment = typeof process.env.NODE_ENV == 'string' ? process.env.NODE_ENV.toLowerCase() : ''
 
 // Check that the current environment is one of the environments above, if not default to staging
 let environmentToExport = typeof environments[currentEnvironment] == 'object' ? environments[currentEnvironment] : environments.staging
 
 //  Export the module
-module.exports  = environmentToExport
+module.exports = environmentToExport
