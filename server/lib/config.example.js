a016ce3539c34c34cd8196a4d64a571903dcf87d
Sciederrick <derrickmbarani@gmail.com>
2021-03-04 12:31:21 +0300

    add config example

---
+++ server/lib/config.example.js
@@ -0,0 +1,53 @@
+/*
+ * Create and export configurtion variables
+ */
+
+ // Container for all the environments
+ let environments = {}
+
+ // Staging (default) environment
+ environments.staging = {
+  httpPort:3000,
+  httpsPort:3001,
+  envName:'staging',
+  hashingSecret:'',
+  twilio:{
+  accountSid : '',
+  authToken : '',
+  fromPhone : ''
+  },
+  database:{
+    name: 'sisibotea',
+    host:'localhost',
+    user:'',
+    password:''
+  },
+ }
+
+ //Production environment
+ environments.production = {
+   httpPort:5000,
+   httpsPort:5001,
+   envName:'production',
+   hashingSecret:'',
+   twilio:{
+    accountSid : '',
+    authToken : '',
+    fromPhone : ''
+  },
+  database:{
+    name: 'sisibotea',
+    host:'localhost',
+    user:'',
+    password:''
+  },
+ }
+
+ // Determine which environment was passed as a command-line argument
+ let currentEnvironment = typeof process.env.NODE_ENV == 'string' ? process.env.NODE_ENV.toLowerCase():''
+
+// Check that the current environment is one of the environments above, if not default to staging
+let environmentToExport = typeof environments[currentEnvironment] == 'object' ? environments[currentEnvironment] : environments.staging
+
+//  Export the module
+module.exports  = environmentToExport
