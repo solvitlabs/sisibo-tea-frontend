eac78bdb4fbb3efd1f0a3a5b04b5b62f843c7158
d3vkk <donaldkk22@yahoo.com>
2021-03-08 10:02:14 +0300

    refactor: Remove home page

--- src/layout/Navbar.vue
+++ src/layout/Navbar.vue
@@ -18,7 +18,7 @@
       <div class="navbar-collapse collapse" id="navbarSidebar">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <router-link class="nav-link" to="/signup">Sign Up</router-link>
+            <router-link class="nav-link" to="/">Sign Up</router-link>
             <router-link class="nav-link" to="/login">Login</router-link>
             <router-link class="nav-link" to="/dashboard"
               >Dashboard</router-link
