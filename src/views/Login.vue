94cafef03924ad7a5658f1393874e5ed2935c62e
d3vkk <donaldkk22@yahoo.com>
2021-03-08 09:56:33 +0300

    refactor: Add navbar to login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,41 +1,49 @@
 <template>
-  <div class="col-md-6 grid-margin stretch-card">
-    <div class="card">
-      <form class="forms-sample" @submit.prevent="login">
-        <div class="card-body">
-          <h4 class="card-title">Login</h4>
-          <div class="form-group">
-            <label for="email">Email address</label>
-            <input
-              type="email"
-              class="form-control"
-              id="email"
-              required
-              v-model="email"
-            />
+  <div>
+    <Navbar />
+    <div class="col-md-6 grid-margin stretch-card">
+      <div class="card">
+        <form class="forms-sample" @submit.prevent="login">
+          <div class="card-body">
+            <h4 class="card-title">Login</h4>
+            <div class="form-group">
+              <label for="email">Email address</label>
+              <input
+                type="email"
+                class="form-control"
+                id="email"
+                required
+                v-model="email"
+              />
+            </div>
+            <div class="form-group">
+              <label for="firstPassword">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="firstPassword"
+                minlength="8"
+                required
+                v-model="password"
+              />
+            </div>
+            <button type="submit" class="btn btn-success mr-2">Login</button>
+            <button class="btn btn-light">Cancel</button>
           </div>
-          <div class="form-group">
-            <label for="firstPassword">Password</label>
-            <input
-              type="password"
-              class="form-control"
-              id="firstPassword"
-              minlength="8"
-              required
-              v-model="password"
-            />
-          </div>
-          <button type="submit" class="btn btn-success mr-2">Login</button>
-          <button class="btn btn-light">Cancel</button>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Navbar from "../layout/Navbar";
+
 export default {
   name: "login",
+  components: {
+    Navbar,
+  },
   data() {
     return {
       email: null,
