2d1f5821ebd4a2e8ca0ba1542987cd9c9a04dce6
d3vkk <donaldkk22@yahoo.com>
2021-04-13 11:17:26 +0300

    refactor: Add navbar and footer to forgotpassword

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,25 +1,36 @@
 <template>
-  <div class="grid-margin stretch-card" id="reset">
-    <div class="col d-flex justify-content-center">
-      <form class="forms-sample shadow" @submit.prevent>
-        <div class="card-body">
-          <h3>Forgot Password</h3>
+  <div>
+    <Navbar />
+    <div class="grid-margin stretch-card" id="reset">
+      <div class="col d-flex justify-content-center">
+        <form class="forms-sample shadow" @submit.prevent>
+          <div class="card-body">
+            <h3>Forgot Password</h3>
             <div class="form-group">
               <label>Email address</label>
               <input type="email" class="form-control form-control-lg" />
             </div>
-            <button type="submit" class="btn btn-success mr-2">Reset password</button>
-        </div>
-      </form>
+            <button type="submit" class="btn btn-success mr-2">
+              Reset password
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-
-
+    <FooterSection />
   </div>
 </template>
 
 <script>
+import Navbar from "../layout/Navbar";
+import FooterSection from "../layout/FooterSection";
+
 export default {
-    name: "password-reset"
+  name: "forgot-password",
+  components: {
+    Navbar,
+    FooterSection,
+  },
 };
 </script>
 
